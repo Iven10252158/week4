d5ade37ac37f30bfae49d8d4d04884eef5667a21
Iven <iven10252158@Ivende-MacBook-Air-2.local>
2020-08-31 22:27:19 +0800

    newhomework

--- all.js
+++ all.js
@@ -1,52 +1,69 @@
 import pagination from './pagination.js';
+import modal from './modal.js';
+import deleteProduct from './deleteProduct.js';
 Vue.component('pagination',pagination);
+Vue.component('modal',modal);
+Vue.component('deleteProduct',deleteProduct);
+Vue.component('loading',VueLoading);
 
 new Vue({
 el:'#app',
 data:{
-    products:{},
+    products:[],
     pagination:{},
     tempProduct:{
-        imageUrl: []
+      imageUrl: [],
     },
+    isLoading:false,
     api: {
     uuid:'35e5ec2f-4b81-4496-9634-fef29022b381',
     apiPath:'https://course-ec-api.hexschool.io/api/'
 },
-    token:'Z2V7AZOtKD42nl1p60maemg7nVFZHrSTaIfdRkqCZOKuKWtPABfcu2Z5pgja',
+    token:'weZhtAxBDMk4MlAVb3yBYdGH7AVXjQrAXmn4HktxoKKd2BfUVLQE2ueGl8vK',
     isNew:''
 },
 methods:{
     getProducts(num=1){
-        console.log(num);
-        const url = `${this.api.apiPath}${this.api.uuid}/admin/ec/products?page${num}=`;
+        // console.log(num);
+        this.isLoading=true;
+        const url = `${this.api.apiPath}${this.api.uuid}/admin/ec/products?page=${num}`;
         axios.get(url)
             .then((res)=>{
-                console.log(res);
-            this.products = res.data.data;
+            this.isLoading=false;
+            console.log(res);
+            this.products = res.data.data; //把遠端的資料套到products上
             this.pagination = res.data.meta.pagination;
+            }).catch((error)=>{
+              this.isLoading=false;
             });
     },
     openModal(isNew, item) {
         switch (isNew) {
           case 'new':
-            this.tempProduct = {};
-            $('#productModal').modal('show');
+              this.tempProduct={
+                imageUrl: [],
+              };
+             $(this.$refs.productModal).modal('show');
             break;
           case 'edit':
-            this.tempProduct = Object.assign({}, item);
-            $('#productModal').modal('show');
+            const url = `${this.api.apiPath}${this.api.uuid}/admin/ec/product/${item.id}`;
+            axios.get(url)
+            .then((res)=>{
+              this.tempProduct = res.data.data;
+              $(this.$refs.productModal).modal('show');
+            })
             break;
           case 'delete':
-            $('#delProductModal').modal('show');
-            this.tempProduct = Object.assign({}, item);
+            $(this.$refs.deleteProduct).modal('show');
+             this.tempProduct = Object.assign({}, item);
             break;
           default:
             break;
         }
 },
+
 },
-created(){
+mounted(){
     // document.cookie = `weekfourToken=${token}; expires=${new Date(expired * 1000)}; path=/`;
     this.token = document.cookie.replace(/(?:(?:^|.*;\s*)weekfourToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
